c9100b2b69efa6fcd2c63a9cb33e9a2be0466014
ValeryTretyak <32965246+ValeryTretyak@users.noreply.github.com>
2024-06-12 10:54:49 +0200

    change to answer 400 for not /hello, build by java 11

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -17,44 +17,68 @@ import com.syndicate.deployment.model.lambda.url.InvokeMode;
 import com.task02.dto.HelloMessage;
 
 @LambdaHandler(lambdaName = "hello_world",
-							 roleName = "hello_world-role",
-							 /*isPublishVersion = true,
-               aliasName = "${lambdas_alias_name}",*/
-							 layers = {"sdk-layer"},
-							 runtime = DeploymentRuntime.JAVA11,
-							 architecture = Architecture.ARM64,
-							 logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+               roleName = "hello_world-role",
+               isPublishVersion = false,
+               /*aliasName = "${lambdas_alias_name}",*/
+               layers = {"sdk-layer"},
+               runtime = DeploymentRuntime.JAVA11,
+               architecture = Architecture.ARM64,
+               logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @LambdaLayer(
-		layerName = "sdk-layer",
-		libraries = {"lib/commons-lang3-3.14.0.jar", "lib/gson-2.10.1.jar"},
-		runtime = DeploymentRuntime.JAVA11,
-		architectures = {Architecture.ARM64},
-		artifactExtension = ArtifactExtension.ZIP
+    layerName = "sdk-layer",
+    libraries = {"lib/commons-lang3-3.14.0.jar", "lib/gson-2.10.1.jar"},
+    runtime = DeploymentRuntime.JAVA11,
+    architectures = {Architecture.ARM64},
+    artifactExtension = ArtifactExtension.ZIP
 )
 @LambdaUrlConfig(
-		authType = AuthType.NONE,
-		invokeMode = InvokeMode.BUFFERED
+    authType = AuthType.NONE,
+    invokeMode = InvokeMode.BUFFERED
 )
-public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class HelloWorld
+    implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	private static final int SC_OK = 200;
-	public static final int SC_SERVER_ERROR = 500;
-	private final Gson gson = new Gson();
+  private static final int SC_OK = 200;
+  public static final int SC_SERVER_ERROR = 500;
+  public static final int BAD_REQUEST = 400;
+  private final Gson gson = new Gson();
 
+  @Override
+  public APIGatewayProxyResponseEvent handleRequest(
+      APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent,
+      Context context) {
+    context.getLogger().log("Val: " + apiGatewayProxyRequestEvent.toString());
+    context.getLogger().log("Val: Path:" + apiGatewayProxyRequestEvent.getPath());
+    context.getLogger().log("Val: Method:" + apiGatewayProxyRequestEvent.getHttpMethod());
+    if (apiGatewayProxyRequestEvent.getRequestContext() == null) {
+      context.getLogger().log("Val: Context is null");
+    } else {
+      context.getLogger()
+          .log("Val: Context.Path:" + apiGatewayProxyRequestEvent.getRequestContext().getPath());
+      context.getLogger().log(
+          "Val: Context.Method:" + apiGatewayProxyRequestEvent.getRequestContext().getHttpMethod());
+    }
+    try {
 
-	@Override
-	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent,
-			Context context) {
-		context.getLogger().log(apiGatewayProxyRequestEvent.toString());
-		try {
-			return new APIGatewayProxyResponseEvent()
-					.withStatusCode(SC_OK)
-					.withBody(gson.toJson(new HelloMessage("Hello from Lambda")));
-		} catch (IllegalArgumentException exception) {
-			context.getLogger().log(exception.getMessage());
-			return new APIGatewayProxyResponseEvent()
-					.withStatusCode(SC_SERVER_ERROR);
-		}
-	}
+      if (!"/hello".equals(apiGatewayProxyRequestEvent.getPath())) {
+        return new APIGatewayProxyResponseEvent()
+            .withStatusCode(BAD_REQUEST)
+            .withBody(gson.toJson(
+                new HelloMessage(BAD_REQUEST,
+                    "Bad request syntax or unsupported method. Request path: "
+                    + apiGatewayProxyRequestEvent.getPath() + ". HTTP method: "
+                    + apiGatewayProxyRequestEvent.getHttpMethod()
+                )));
+      }
+
+      return new APIGatewayProxyResponseEvent()
+          .withStatusCode(SC_OK)
+          .withBody(gson.toJson(new HelloMessage("Hello from Lambda")));
+    } catch (IllegalArgumentException exception) {
+      context.getLogger().log(exception.getMessage());
+      return new APIGatewayProxyResponseEvent()
+          .withStatusCode(SC_SERVER_ERROR);
+    }
+  }
 }
--- task02/jsrc/main/java/com/task02/dto/HelloMessage.java
+++ task02/jsrc/main/java/com/task02/dto/HelloMessage.java
@@ -1,10 +1,15 @@
 package com.task02.dto;
 
 public class HelloMessage {
-  private final int statusCode = 200;
+  private final int statusCode;
   private final String message;
 
   public HelloMessage(String message) {
+    this(200, message);
+  }
+
+  public HelloMessage(int statusCode, String message) {
+    this.statusCode = statusCode;
     this.message = message;
   }
 }
