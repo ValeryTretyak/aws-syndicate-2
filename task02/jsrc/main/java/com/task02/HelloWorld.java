e59a142fd19e979a2bbffe220ae51374f8ab8d42
ValeryTretyak <32965246+ValeryTretyak@users.noreply.github.com>
2024-06-12 12:32:15 +0200

    reduce params in lambda definition

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -6,36 +6,16 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.google.gson.Gson;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
-import com.syndicate.deployment.model.Architecture;
-import com.syndicate.deployment.model.ArtifactExtension;
-import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
-import com.syndicate.deployment.model.lambda.url.AuthType;
-import com.syndicate.deployment.model.lambda.url.InvokeMode;
 import com.task02.dto.HelloMessage;
 
 @LambdaHandler(lambdaName = "hello_world",
                roleName = "hello_world-role",
                isPublishVersion = false,
-               /*aliasName = "${lambdas_alias_name}",*/
-               layers = {"sdk-layer"},
-               runtime = DeploymentRuntime.JAVA11,
-               architecture = Architecture.ARM64,
                logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@LambdaLayer(
-    layerName = "sdk-layer",
-    libraries = {"lib/commons-lang3-3.14.0.jar", "lib/gson-2.10.1.jar"},
-    runtime = DeploymentRuntime.JAVA11,
-    architectures = {Architecture.ARM64},
-    artifactExtension = ArtifactExtension.ZIP
-)
-@LambdaUrlConfig(
-    authType = AuthType.NONE,
-    invokeMode = InvokeMode.BUFFERED
-)
+@LambdaUrlConfig
 public class HelloWorld
     implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
